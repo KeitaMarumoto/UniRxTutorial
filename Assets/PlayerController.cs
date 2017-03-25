2bec58b3b3ec56dac3f7d3a3b0542bfe85d7bc84
KeitaMarumoto <marumoto.keita@gmail.com>
2017-03-26 00:29:29 +0900

    一定時間ごとに処理をするコードを記述。
    一定時間ごとにtransform.position.yを監視して、一定以下の座標になったらDestroy。

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
+using System.Linq;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	void Start()
+	{
+		Observable
+			.Interval(TimeSpan.FromSeconds(0.3f))
+			.Subscribe(_ => Debug.Log("position.y = " + transform.position.y))
+			.AddTo(this);
+
+		Observable
+			.Interval(TimeSpan.FromSeconds(1.5f))
+			.Subscribe(_ => Debug.Log("Update"))
+			.AddTo(this);
+
+		//PlayerのPositionを一定時間おきに監視して、指定以下になったらDestroyする。
+		Observable
+			.Interval(TimeSpan.FromSeconds(1.5f))
+			.Where(_ => transform.position.y < -20.0f)
+			.Subscribe(_ =>
+			{
+				Debug.Log("Player Deleted!!");
+				Destroy(gameObject);
+			}).AddTo(this);
+	}
+}
